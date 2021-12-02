328c929be9ddbfe7752e683488ce529bf4d7ceac
NGnius <ngniusness@gmail.com>
2021-12-02 17:47:12 -0500

    Improve compile errors with proc_macro_error::abort!

--- members/macro_parent/src/lib.rs
+++ members/macro_parent/src/lib.rs
@@ -4,11 +4,14 @@ extern crate proc_macro;
 
 use proc_macro::{TokenStream};
 
+use proc_macro_error::{abort, proc_macro_error};
+
 use macro_common::Generate;
 
 /// Parent macro for Roopert.
 /// This is the shorter equivalent to `#[roopert(parent)]`.
 /// Refer to the top-level doc for documentation of this attribute macro.
+#[proc_macro_error]
 #[proc_macro_attribute]
 pub fn parent(attr: TokenStream, item: TokenStream) -> TokenStream {
     //let ast: &DeriveInput = &syn::parse(item.clone()).expect("Unable to parse input target");
@@ -17,7 +20,7 @@ pub fn parent(attr: TokenStream, item: TokenStream) -> TokenStream {
     println!("Parsed parent attribute: {:?}", attr);
     match attr.generate_auto(item.into()) {
         Ok(stream) => stream.into(),
-        Err(msg) => panic!("{}", msg)
+        Err(msg) => abort!("{}", msg)
     }
     //let output = quote! {};
     //TokenStream::from(output)
--- members/macro_root/src/lib.rs
+++ members/macro_root/src/lib.rs
@@ -4,12 +4,15 @@
 
 use proc_macro::TokenStream;
 
+use proc_macro_error::{abort, proc_macro_error};
+
 use macro_common::{RoopertAttribute, Generate};
 
 /// Root macro for Roopert.
 /// All attribute macros are of the form `#[roopert(type)]`,
 /// where `type` is the type of attribute.
 /// Refer to the top-level doc for supported types.
+#[proc_macro_error]
 #[proc_macro_attribute]
 pub fn roopert(attr: TokenStream, item: TokenStream) -> TokenStream {
     //let ast: &DeriveInput = &syn::parse(item.clone()).expect("Unable to parse input target");
@@ -18,7 +21,7 @@ pub fn roopert(attr: TokenStream, item: TokenStream) -> TokenStream {
     println!("Parsed roopert attribute: {:?}", attr);
     match attr.generate_auto(item.into()) {
         Ok(stream) => stream.into(),
-        Err(msg) => panic!("{}", msg)
+        Err(msg) => abort!("{}", msg)
     }
     //let output = quote! {};
     //TokenStream::from(output)
